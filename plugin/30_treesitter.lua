e38c15496f17cae6ed8da3fd51e6b91ef3b83366
Mohamad Supangat <moha.supangat@gmail.com>
2026-04-15 13:50:40 +0700

    feat: mirgating to neovim-treesitter

--- plugin/30_treesitter.lua
+++ plugin/30_treesitter.lua
@@ -2,42 +2,66 @@ local add = MiniDeps.add
 local now_if_args = Config.now_if_args
 
 now_if_args(function()
-  add('romus204/tree-sitter-manager.nvim')
+  add({
+    source = 'neovim-treesitter/nvim-treesitter',
+    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+    build = ':TSUpdate',
+  })
+  add('neovim-treesitter/treesitter-parser-registry')
+
+
+
   add('windwp/nvim-ts-autotag')
-  -- add("folke/ts-comments.nvim")
   add("nvim-treesitter/nvim-treesitter-context")
   add("JoosepAlviste/nvim-ts-context-commentstring")
 
-  require("tree-sitter-manager").setup({
-    ensure_installed = {
-      'lua',
-      "markdown_inline",
-      "vimdoc",
-      "lua",
-      "typescript",
-      "vue",
-      "pug",
-      "python",
-      "php",
-      "phpdoc",
-      "prisma",
-      "markdown",
-      "html",
-      "blade",
-      "vim",
-      "json",
-      "css",
-      "dockerfile",
-      "bash",
-      "fish",
-      "javascript",
-      "scss",
-      "http",
-      "xml",
-      "yaml",
-    },
-    highlight = true,
-  })
+
+  local languages = {
+    'lua',
+    "markdown_inline",
+    "vimdoc",
+    "lua",
+    "typescript",
+    "vue",
+    "pug",
+    "python",
+    "php",
+    "phpdoc",
+    "prisma",
+    "markdown",
+    "html",
+    "blade",
+    "vim",
+    "json",
+    "css",
+    "dockerfile",
+    "bash",
+    "fish",
+    "javascript",
+    "scss",
+    "http",
+    "xml",
+    "yaml",
+
+  }
+  local isnt_installed = function(lang)
+    return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
+  end
+  local to_install = vim.tbl_filter(isnt_installed, languages)
+  if #to_install > 0 then require('nvim-treesitter').install(to_install) end
+
+  -- Enable tree-sitter after opening a file for a target language
+  local filetypes = {}
+  for _, lang in ipairs(languages) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+      table.insert(filetypes, ft)
+    end
+  end
+  local ts_start = function(ev)
+    vim.treesitter.start(ev.buf)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+  Config.new_autocmd('FileType', filetypes, ts_start, 'Start tree-sitter')
 
   require('ts_context_commentstring').setup({
     enable_autocmd = false,
