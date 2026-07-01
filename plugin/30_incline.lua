96dd61be92cf63a5b3b681f533ff5c5a3dbeb468
Mohamad Supangat <moha.supangat@gmail.com>
2026-07-01 21:59:19 +0700

    disbale navic i don't like

--- plugin/30_incline.lua
+++ plugin/30_incline.lua
@@ -2,11 +2,9 @@ local add = MiniDeps.add
 local now_if_args = Config.now_if_args
 
 now_if_args(function()
-	add("https://github.com/SmiteshP/nvim-navic")
 	add("https://github.com/b0o/incline.nvim")
 
 	local helpers = require("incline.helpers")
-	local navic = require("nvim-navic")
 	require("incline").setup({
 		window = {
 			padding = 0,
@@ -25,16 +23,6 @@ now_if_args(function()
 				{ filename, gui = modified and "bold,italic" or "bold" },
 				-- guibg = "#44406e",
 			}
-			if props.focused then
-				for _, item in ipairs(navic.get_data(props.buf) or {}) do
-					table.insert(res, {
-						{ " > ", group = "NavicSeparator" },
-						{ item.icon, group = "NavicIcons" .. item.type },
-						{ item.name, group = "NavicText" },
-					})
-				end
-			end
-			table.insert(res, " ")
 			return res
 		end,
 	})
